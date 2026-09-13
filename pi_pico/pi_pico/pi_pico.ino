1db0b37305fe17553b297a6f36eca1e23c869b6b
grover-dev <timkouzmenkov@gmail.com>
2026-09-13 01:35:37 -0700

    rust is gonna make me crash out

---
+++ pi_pico/pi_pico/pi_pico.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  // - set up serial connection:
+  //   - read back commands
+  //   - stream telemetry (commanded speed, eventually ina measurements)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ src/bms.rs
@@ -0,0 +1,14 @@
+/**
+ * Driver for the Dyness 12V Smart Battery
+ * Im cheap so i picked the cheap battery with the bluetooth BMS, thus we now do jank shit to deal with it
+ */
+
+/*
+ * TODO: Connect to bluetooth (allow for retries?) -> send command to read data -> parse response
+ */
+
+struct DynessBms {
+    // FIXME: Need the bluetooth object
+}
+
+impl DynessBms {}
