37d0c170989939e85810422769cbfa2652bc9951
grover-dev <timkouzmenkov@gmail.com>
2026-09-13 01:53:55 -0700

    c++ is so easy

--- pi_pico/pi_pico/pi_pico.ino
+++ pi_pico/pi_pico/pi_pico.ino
@@ -1,12 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "structs.h"
+#include <cstring>
+#include <limits>
 
-  // - set up serial connection:
-  //   - read back commands
-  //   - stream telemetry (commanded speed, eventually ina measurements)
+constexpr int PwmPin = 15;
+
+void setup() {
+  pinMode(PwmPin, OUTPUT);
+  analogWrite(PwmPin, 0);
+
+  // This is running over the virtual port, baud rate is meaningless
+  Serial.begin(115200);
+
+  /* Not sure if this is actually reqiured?  */
+  // while (!Serial) {
+  // }
+
+  Serial.println("Raspberry Pi Pico Serial Initialized!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // FIXME: Ingest the commands -> update local state
+  static state_t state{};
 
+  // FIXME: read command -> byte by byte?
+
+  const uint8_t duty_cycle =
+      state.commanded_motor_duty_cycle * std::numeric_limits<uint8_t>::max();
+  analogWrite(PwmPin, duty_cycle);
+
+  // TODO: measure ina's and shit
+  Serial.write((uint8_t *)&state, sizeof(state_t));
 }
